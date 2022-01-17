6152f4b976f5a2613e8f71d0fe6f611d3cf5a7ef
yaevinn <pietrzak.mateusz@outlook.com>
2022-01-17 23:09:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# LANscanner
+
+Python script to scanning open TCP ports in networks.
+Script is running in threads.
+
+## Usage
+
+1. Definie network address, ports and number of threads in portScanner.py
+2. Run portScanner.py
+3. Read output
---
+++ scanner.py
@@ -0,0 +1,18 @@
+import socket
+
+# scan port on ip
+# map param should looks like eg.: [ 'ip': '127.0.0.1', 'port': 80 ]
+def portscan(map):
+    ip = map['ip']
+    port = map['port']
+    # longer timeout -> more sensitive scanner
+    # shorter timeout -> faster
+    # 0.25 is optimal
+    socket.setdefaulttimeout(0.25)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        conn = sock.connect((str(ip), port))
+        print('Found! IP:', ip, 'port', port, 'is open')
+        conn.close()
+    except:
+        pass
